37a76e4a59ba9688b1c29f849eb70e72bd7aff0d
Aixa <aixacasanova@gmail.com>
2016-09-15 13:57:02 -0300

    Se agrega juegos
    Se agrega juegos

--- Clase4/PHP_ABM_Persona-1.0.4/js/miaplicacion.js
+++ Clase4/PHP_ABM_Persona-1.0.4/js/miaplicacion.js
@@ -45,6 +45,12 @@ $stateProvider
 			templateUrl:"login.html",
 			controller:"ctrllogin"}
 			)
+	.state(
+			"juego",
+			{url:"/juego",
+			templateUrl:"AdivinaElNumero2.html",
+			controller:"Control"}
+			)
 		.state(
 			"regist",
 			{url:"/regist",
@@ -87,6 +93,58 @@ miapp.controller("ctrlregist", function($scope){
 $scope.mail="";
 
 });
+
+
+
+miapp.controller("Control", function($scope){
+ 
+$scope.num="0";
+$scope.intentos=0;
+$scope.NSecreto="";
+$scope.result="";
+$scope.Falta="";
+$scope.result2="";
+ 
+	$scope.verificar=function()
+	{
+		$scope.intentos=$scope.intentos+1;
+		if ($scope.num == $scope.NSecreto)
+		{
+			if($scope.intentos == '1'){$scope.result2="usted es un Psíquico";}
+			if($scope.intentos == '2'){$scope.result2="excelente percepción";}
+			if($scope.intentos == '3'){$scope.result2="Esto es suerte";}
+			if($scope.intentos == '4'){$scope.result2="Excelente técnica";}
+			if($scope.intentos == '5'){$scope.result2="usted está en la media";}
+			if($scope.intentos >= '6' && $scope.intentos < '10'){$scope.result2="falta técnica";}
+			if($scope.intentos > '10') {$scope.result2="afortunado en el amor!";}
+			$scope.result="Usted es un ganador!!! y nro de intentos: "+$scope.intentos;
+
+		}else{
+			if ($scope.NSecreto>$scope.num) 
+			{
+				$scope.Falta=$scope.NSecreto - $scope.num; 
+				$scope.result="Numero incorrecto, Falta "+ $scope.Falta;
+			}else{
+				$scope.Falta=$scope.num - $scope.NSecreto;
+				$scope.result="Numero incorrecto, Sobra "+ $scope.Falta;
+			} 
+			
+		}
+	}
+	$scope.comenzar=function()
+	{
+		$scope.intentos=0;
+		$scope.num="0";
+		$scope.NSecreto= Math.round(Math.random()*100);
+		$scope.result="Numero secreto generado, Comienza!";
+		$scope.result2=" ";
+	
+	}
+
+
+
+});
+
 
 
 
